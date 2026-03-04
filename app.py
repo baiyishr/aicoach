8734bdd45a6181ae8e8822cb9b9b40b315b13910
Xiaobing Li <baiyishr@gmail.com>
2026-03-03 23:00:51 -0500

    Change Uvicorn host and port settings

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ Entry point. Run with: python app.py
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8501, reload=True)
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
